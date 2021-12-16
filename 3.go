66c83c291953ccbde9be6aa62e4925c0874ba50a
unknown <luokai@luokai-cy.cis.com>
2021-12-16 15:56:47 +0800

    secondcommit

--- 3.go
+++ 3.go
@@ -1,65 +1,83 @@
 package main
 
-type TreeNode struct {
-	val   int
-	left  *TreeNode
-	right *TreeNode
+import (
+	"fmt"
+)
+
+type Node struct {
+	value    int
+	priority int
 }
 
-func preOrder(root *TreeNode) []int {
-	if root == nil {
-		return nil
-	}
-	if root.left == nil && root.right == nil {
-		return []int{root.val}
-	}
-	var stack []*TreeNode
-	var res []int
-	stack = append(stack, root)
-	for len(stack) != 0 {
-		i := stack[len(stack)-1]
-		stack = stack[:len(stack)-1]
-		res = append(res, i.val)
-		if i.right != nil {
-			stack = append(stack, i.right)
-		}
-		if i.left != nil {
-			stack = append(stack, i.left)
-		}
-	}
-	return res
+type Queue struct {
+	heap     []Node
+	capacity int
+	used     int
 }
 
-func inorder(root *TreeNode) []int {
-	if root == nil {
-		return nil
+func NewPriorityQueue(capacity int) Queue {
+	return Queue{
+		heap:     make([]Node, capacity+1, capacity+1),
+		capacity: capacity,
+		used:     0,
 	}
-	if root.left == nil && root.right == nil {
-		return []int{root.val}
-	}
-	res := inorder(root.left)
-	res = append(res, root.val)
-	res = append(res, inorder(root.right)...)
-	return res
 }
 
-func postOrder(root *TreeNode) []int {
-	if root == nil {
-		return nil
+func (q *Queue) Push(node Node) {
+	if q.used >= q.capacity {
+		return
 	}
-	var res []int
-	if root.left != nil {
-		lres := postOrder(root.left)
-		if len(lres) > 0 {
-			res = append(res, lres...)
+	q.used++
+	q.heap[q.used] = node
+}
+
+func (q *Queue) Pop() Node {
+	if q.used == 0 {
+		return Node{-1, -1}
+	}
+	adjustNode(q.heap, 1, q.used)
+	node := q.heap[1]
+	q.heap[1] = q.heap[q.used]
+	q.used--
+	return node
+}
+
+func (q *Queue) Top() Node {
+	if q.used == 0 {
+		return Node{-1, -1}
+	}
+	adjustNode(q.heap, 1, q.used)
+	return q.heap[1]
+}
+
+func adjustNode(heap []Node, start, end int) {
+	if start >= end {
+		return
+	}
+	for i := end / 2; i >= start; i-- {
+		high := i
+		if heap[high].priority < heap[2*i].priority {
+			high = 2 * i
 		}
-	}
-	if root.right != nil {
-		rres := postOrder(root.right)
-		if len(rres) > 0 {
-			res = append(res, rres...)
+		if 2*i+1 <= end && heap[high].priority < heap[2*i+1].priority {
+			high = 2 * i
 		}
+		if high == i {
+			continue
+		}
+		heap[high], heap[i] = heap[i], heap[high]
 	}
-	res = append(res, root.val)
-	return res
+}
+
+func main() {
+	queue := NewPriorityQueue(7)
+	queue.Push(Node{0, 1})
+	queue.Push(Node{3, 1})
+	queue.Push(Node{3, 2})
+	queue.Push(Node{6, 6})
+	queue.Push(Node{12, 5})
+	queue.Push(Node{13, 8})
+	fmt.Println(queue)
+	queue.Pop()
+	fmt.Println(queue)
 }
